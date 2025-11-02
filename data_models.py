808768902b2dd6f6c5aa31d7e23f4d851258a6d2
jasonbdt <ja.bladt@gmail.com>
2025-11-02 09:39:06 +0100

    Add data models

---
+++ data_models.py
@@ -0,0 +1,39 @@
+from typing import Self
+
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+db = SQLAlchemy(model_class=Base)
+
+
+class Author(db.Model):
+    __tablename__ = 'authors'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String)
+    birth_date: Mapped[DateTime] = mapped_column(DateTime)
+    date_of_death: Mapped[DateTime] = mapped_column(DateTime)
+
+    def __repr__(self: Self) -> str:
+        return f"Author(id = {self.id}, " \
+               f"name = {self.name})"
+
+
+class Book(db.Model):
+    __tablename__ = 'books'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    isbn: Mapped[str] = mapped_column(String)
+    title: Mapped[str] = mapped_column(String)
+    publication_year: Mapped[DateTime] = mapped_column(DateTime)
+    author_id: Mapped[int] = mapped_column(ForeignKey("authors.id"))
+
+    def __repr__(self: Self) -> str:
+        return f"Book(id = {self.id}, " \
+               f"title = {self.title})"
